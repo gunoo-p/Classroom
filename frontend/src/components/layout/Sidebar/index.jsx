8f0c2d8a05c5ae92333a45b88a539d4385d385be
gunoo <gunwoo405@naver.com>
2025-12-10 21:36:56 +0900

    3Day

---
+++ frontend/src/components/layout/Sidebar/index.jsx
@@ -0,0 +1,15 @@
+export default function sidebar() {
+  return (
+    <div>
+      <div>
+        <div>로고</div>
+        <p>대시보드</p>
+      </div>
+
+      <div>
+        <div>로그인 사용자 정보</div>
+        <button>로그아웃</button>
+      </div>
+    </div>
+  );
+}
